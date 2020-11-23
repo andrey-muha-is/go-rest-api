35890276133fc1ee9b9cdfe9c03203a3d465e8eb
Andrey Muha <andrey.muha@intexsof.by>
2020-11-23 14:47:58 +0300

    Added chi and chi/render, added routes, updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM golang:latest
+# Download app dependencies
+RUN go get github.com/go-chi/chi
+RUN go get github.com/go-chi/render
+
 RUN mkdir /app
 ADD . /app/
 WORKDIR /app
--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
 )
 
 func main() {
-	fmt.Println("Hello, World")
+	r := chi.NewRouter()
+
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	r.Route("/channels", channelsRoutes)
+
+	http.ListenAndServe(":3000", r)
+}
+
+func channelsRoutes(r chi.Router) {
+	r.Get("/", GetChannels)
+	r.Get("/{channelID}/programs", GetChannelPrograms)
+}
+
+func GetChannels(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get list of all channels")
+}
+
+func GetChannelPrograms(w http.ResponseWriter, r *http.Request) {
+	channelID := chi.URLParam(r, "channelID")
+
+	fmt.Fprintf(w, "Get list of programs for channel %s", channelID)
 }
